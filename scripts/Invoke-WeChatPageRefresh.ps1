6139c24b52690bea60c48b64f004d4b68f1850d6
wubeizhiguang <3432937651@qq.com>
2026-08-25 20:00:18 +0800

    feat: refresh WeChat page before bridge readiness

---
+++ scripts/Invoke-WeChatPageRefresh.ps1
@@ -0,0 +1,61 @@
+[CmdletBinding()]
+param()
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+if (-not ('TrendRadar.WeChatWindow' -as [type])) {
+    Add-Type -TypeDefinition @'
+using System;
+using System.Runtime.InteropServices;
+
+namespace TrendRadar {
+    public static class WeChatWindow {
+        public const uint WM_KEYDOWN = 0x0100;
+        public const uint WM_KEYUP = 0x0101;
+        public const uint VK_F5 = 0x74;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool PostMessage(IntPtr hWnd, uint message, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+    }
+}
+'@ -ErrorAction Stop
+}
+
+$processNames = @('Weixin', 'WeChat', 'WeChatAppEx')
+$candidates = @(
+    foreach ($name in $processNames) {
+        foreach ($process in @(Get-Process -Name $name -ErrorAction SilentlyContinue)) {
+            try {
+                if ($process.MainWindowHandle -eq [IntPtr]::Zero) { continue }
+                if (-not [TrendRadar.WeChatWindow]::IsWindowVisible($process.MainWindowHandle)) { continue }
+                if ([string]::IsNullOrWhiteSpace([string]$process.MainWindowTitle)) { continue }
+                [pscustomobject]@{ Handle = $process.MainWindowHandle }
+            } catch { }
+        }
+    }
+)
+
+if ($candidates.Count -eq 0) { throw 'wechat_window_not_found' }
+
+$foreground = [TrendRadar.WeChatWindow]::GetForegroundWindow()
+$foregroundCandidates = @($candidates | Where-Object { $_.Handle -eq $foreground })
+if ($foregroundCandidates.Count -eq 1) {
+    $selected = $foregroundCandidates[0]
+} elseif ($candidates.Count -eq 1) {
+    $selected = $candidates[0]
+} else {
+    throw 'wechat_window_ambiguous'
+}
+
+if (-not [TrendRadar.WeChatWindow]::PostMessage($selected.Handle, [TrendRadar.WeChatWindow]::WM_KEYDOWN, [IntPtr][TrendRadar.WeChatWindow]::VK_F5, [IntPtr]::Zero)) {
+    throw 'wechat_page_refresh_failed'
+}
+[void][TrendRadar.WeChatWindow]::PostMessage($selected.Handle, [TrendRadar.WeChatWindow]::WM_KEYUP, [IntPtr][TrendRadar.WeChatWindow]::VK_F5, [IntPtr]::Zero)
+'wechat_page_refresh_sent'
